2a3dabbacb8911adb5ca426da86454724322e498
Nick Kinlen <nick_kinlen@hotmail.com>
2018-03-20 19:16:58 -0400

    Both input boxes working

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ export default class App extends Component {
     super(props);
     this.state = {
       items: ["Pumpkin Pie", "Spaghetti", "Onion Pie"],
+      ingredients:[
+        ["Pumpkin Puree", "Sweetened Condensed Milk", "Eggs", "Pumpkin Pie Spice", "Pie Crust"],
+        ["Noodles", "Tomato Sauce", "(Optional) Meatballs"],
+        ["Onion", "Pie Crust"]
+      ],
       inputVal: '',
+      ingredientVal: '',
       showRecipe: false
     };
   }
@@ -17,11 +23,8 @@ export default class App extends Component {
     this.setState({inputVal: event.target.value});
   };
 
-  // Shows recipe
-  AddRecipe = (bool) => {
-    this.setState({
-      showRecipe: bool
-    });
+  handleIngredientChange = (event) => {
+    this.setState({ingredientVal: event.target.value});
   }
 
   // When user submits recipe this adds it
@@ -29,7 +32,24 @@ export default class App extends Component {
     event.preventDefault()
     this.setState({
       inputVal: '',
-      items: [...this.state.items, this.state.inputVal]
+      items: [...this.state.items, this.state.inputVal],
+      ingredientVal: '',
+      ingredients: [...this.state.ingredients, this.state.ingredientVal]
+    });
+  }
+
+  onIngredientSubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      ingredientVal: '',
+      ingredients: [...this.state.ingredients, this.state.ingredientVal]
+    });
+  }
+
+  // Shows recipe
+  AddRecipe = (bool) => {
+    this.setState({
+      showRecipe: bool
     });
   }
 
@@ -37,16 +57,30 @@ export default class App extends Component {
     return (
       <div className="App">
         <h3>Recipe List</h3>
-        <RecipeList items={this.state.items} />
+        <RecipeList items={this.state.items} ingredients={this.state.ingredients} />
+        <button onClick={this.AddRecipe}>Add New Recipe</button>
 
 
       { this.state.showRecipe ?
-        <form className="App" onSubmit={this.onSubmit}>
-        <input value={this.state.inputVal}
-        onChange={this.handleChange} />
-        <button>Submit</button>
-        {/*<button onClick={this.AddRecipe.bind(null, true)}>Add Recipe</button>*/}
-        </form>
+        <div>
+          <form className="Recipe-List" onSubmit={this.onSubmit}>
+            <div className="Recipe-Item">
+              <label>Recipe Name</label>
+              <input
+                value={this.state.inputVal}
+                onChange={this.handleChange} />
+            </div>
+
+            <div className="Recipe-Item">
+              <label>Ingredients</label>
+              <input
+                value={this.state.ingredientVal}
+                onChange={this.handleIngredientChange} />
+            </div>
+            <button>Submit</button>
+          </form>
+        </div>
+
         :null
       }
       </div>
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 const RecipeList = props => (
-    <ul>
+  <div>
+    <ul className="Recipe-List">
       {
         props.items.map((item, index) => <li key={index}>{item}</li>)
       }
     </ul>
+    <ul>
+    {
+      //props.ingredients.map((item, index) => <li key={index}>{item}</li>)
+
+    }
+    </ul>
+  </div>
 );
 
 export default RecipeList;
